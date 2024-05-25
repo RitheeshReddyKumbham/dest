f6ef8fb5c1e0f16cb6d36a532de9f0496dacf51b
RitheeshReddyKumbham <ritheeshreddy690@gmail.com>
2024-05-25 09:27:29 +0000

    commit messge

---
+++ src/components/DestinationItem/index.js
@@ -0,0 +1,14 @@
+// Write your code here
+import './index.css'
+
+const DestinationItem = props => {
+  const {destinationDetails} = props
+  const {imgUrl, name} = destinationDetails
+  return (
+    <li className="item">
+      <img className="img" src={imgUrl} alt={name} />
+      <p className="name">{name}</p>
+    </li>
+  )
+}
+export default DestinationItem
---
+++ src/components/DestinationSearch/index.js
@@ -0,0 +1,40 @@
+// Write your code here
+import {Component} from 'react'
+
+import DestinationItem from '../DestinationItem'
+
+import './index.css'
+
+class DestinationSearch extends Component {
+  state = {
+    searchInput: '',
+  }
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+  render() {
+    const {searchInput} = this.state
+    const {destnationsList} = this.props
+    const searchResults = destnationsList.filter(eachDestination =>
+      eachDestination.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    return (
+      <div className="main">
+        <h1 className="head">Destination Search</h1>
+        <input
+          type="search"
+          className="searchIp"
+          value={searchInput}
+          placeholder="search"
+          onChange={this.onChangeSearchInput}
+        />
+        <ul>
+          {searchResults.map(eachDestination => (
+            <DestinationItem />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+export default DestinationSearch
